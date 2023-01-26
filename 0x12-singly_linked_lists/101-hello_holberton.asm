02f0836c30c7817f8efa4ee26fa92f239b97c3df
chaymaeedd <c.eddouks@gmail.com>
2023-01-26 06:50:20 -0800

    Update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,91 +1,26 @@
+; Comment: writing in assembly language
+; File: 101-hello_holberton.asm
+; Auth: Chaymae
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
 
-indef MAIN_H
+extern printf
 
-#define MAIN_H
+section .text
+	global main
+main:
+push rbp
 
-#include <stdarg.h function, to be called
+mov rdi,fmt
+mov rsi,msg
+ mov rax,0
+call printf
 
+pop rbp
 
+mov rax,0
+ret
 
-	        section .data		; Data section, initialized variables
-
-msg:		db "Hello, Holberton", 0 ; C string needs 0
-
-fmt:	    	db "%s", 10, 0          ; The printf format, "\n",'0'
-
-
-
-	        section .text		; Code section.
-
-
-
-	        global main		; the standard gcc entry point
-
-main:					; the program label for the entry point
-
-	        push    rbp		; set up stack frame, must be alligned
-
-
-
-		mov	rdi,fmt
-
-		mov	rsi,msg
-
-		mov	rax,0		; or can be  xor  rax,rax
-
-	        call    printf		; Call C function
-
-
-
-		pop	rbp		; restore stack
-
-
-
-		mov	rax,0		; normal, no error, return value
-
-		ret			; return
-
-#include <stdio.h>
-
-#include <unistd.h>
-
-
-
-#define UNUSED(x) (void)(x)
-
-#define BUFF_SIZE 1024
-
-
-
-/* FLAGS */
-
-#define F_MINUS 1
-
-#define F_PLUS 2
-
-#define F_ZERO 4
-
-#define F_HASH 8
-
-#define F_SPACE 16
-
-
-
-/* SIZES */
-
-#define S_LONG 2
-
-#define S_SHORT 1
-
-
-
-/**
-
- * struct fmt - Struct op
-
- *
-
- * @fmt: The format.
-
- * @fn: The function associated.
-
+section .data
+	msg: db "Hello, Holberton", 0
+	fmt: db "%s", 10, 0
